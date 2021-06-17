12ce014f7d5250bde8a44321d376b7c823b29f4c
NareenFyp <81482717+NareenFyp@users.noreply.github.com>
2021-06-17 23:11:49 +0100

    Delete components directory

--- components/actions/action-types/cart-actions.js
+++
@@ -1,9 +0,0 @@
-
-//Precautionary measures to avoid errors of misspelling etc, so defined by string constants
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-export const SUB_QUANTITY = 'SUB_QUANTITY';
-export const ADD_QUANTITY = 'ADD_QUANTITY';
-export const ADD_VAT= 'ADD_VAT';
-export const SUB_VAT= 'SUB_VAT';
-
